37e69ce5511fb948c7a672c7338c96197f3b2454
VedantDhoke <142344095+VedantDhoke@users.noreply.github.com>
2023-09-24 13:39:03 +0530

    Add files via upload

---
+++ NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,18 @@
+
+package javaapplication1;
+
+
+
+import com.login.LoginFrame;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+      //create object of that login frame run this project login frame must show first...
+      
+     LoginFrame loginObj = new LoginFrame();
+     loginObj.show();
+      
+    }
+    
+}
